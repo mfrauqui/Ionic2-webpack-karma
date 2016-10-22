03b87acdc471809707dc1ad2e7525744c6225dd6
= <=>
2016-10-22 23:02:25 +0600

    ts config: Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component'; 
-import { TabsPage } from '../pages/tabs/tabs';
-
-//import { ProfilePage } from '../pages/profile/profile'
+import { TabsPage } from '../pages/tabs/tabs'; 
 import {LoginPage} from '../pages/login/login';
 import { QuotesPage } from '../pages/quotes/quotes'
 
@@ -11,18 +9,12 @@ import { AuthConfig, AuthHttp } from 'angular2-jwt';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage';
 
-let storage: Storage = new Storage();
-
-export function getAuthHttp(http) {
-  return new AuthHttp(new AuthConfig({
-    globalHeaders: [{'Accept': 'application/json'}],
-    tokenGetter: (() => storage.get('id_token'))
-  }), http);
-}
+ 
+ 
 
 @NgModule({
   declarations: [
-    MyApp, 
+    MyApp,
     LoginPage,
     QuotesPage,
     TabsPage
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -10,35 +10,24 @@ const rootDir = path.resolve(__dirname, '..');
 module.exports = {
     debug: true,
     devServer: {
-        contentBase: path.resolve(rootDir, 'www/build'),
+        contentBase: path.resolve(rootDir, 'dist'),
         port: 9000
     },
     devtool: 'source-map',
 
     entry: {
-        app: [ path.resolve(rootDir, 'src', 'app/main.dev') ],
-        vendor: [ path.resolve(rootDir, 'src', 'app/main.prod') ]
+        app: [ path.resolve(rootDir, 'src', 'app/main.prod.ts') ],
+       // vendor: [ path.resolve(rootDir, 'src', 'vendor') ]
     },
      module: {
         loaders: [
             { loader: 'raw', test: /\.(css|html)$/ },
-            { exclude: /node_modules/, loader: 'ts', test: /\.ts$/ },
-            {
-    test: /\.js$/,
-    loader: 'babel-loader',
-    include: [
-        path.join(__dirname, 'app'),
-        path.join(__dirname, 'test')
-    ],
-    exclude: path.join(__dirname, 'node_modules')
-},
+            { exclude: /node_modules/, loader: 'ts', test: /\.ts$/ }
         ]
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(rootDir, 'www/build'),
-        sourceMapFilename: '[name].map',
-        chunkFilename: '[id].chunk.js'
+        path: path.resolve(rootDir, 'dist')
     },
 plugins: [
         new ChunkWebpack({
@@ -52,20 +41,7 @@ plugins: [
             template: path.resolve(rootDir, 'src', 'index.html')
         })
     ],
-    tslint: {
-    emitErrors: false,
-    failOnHint: false,
-    resourcePath: 'app'
-  },
      resolve: {
         extensions: [ '', '.js', '.ts' ]
-    },
-    node: {
-    global: 'window',
-    crypto: 'empty',
-    process: true,
-    module: false,
-    clearImmediate: false,
-    setImmediate: false
-  }
+    }
 };
